3a0d6ef0660f9e67e3f4e896f5302be833a8b103
Bryan Kauan <165050307+braiatenebras@users.noreply.github.com>
2025-09-15 09:38:25 -0300

    Melhorando a page Carros.
    Adicionei os primeiros carros que serao listados em json e vou começar a melhorar logo logo.

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// estilos gerais das pages
+
+// FADE-IN AO SCROLL
+const faders = document.querySelectorAll('.fade-in');
+
+const options = {
+    threshold: 0.1
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+            observer.unobserve(entry.target);
+        }
+    });
+}, options);
+
+faders.forEach(fader => {
+    observer.observe(fader);
+});
+
+// menu hamburguer
+const hamburger = document.querySelector('.hamburger');
+const navbar = document.querySelector('.navbar');
+
+hamburger.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+});
